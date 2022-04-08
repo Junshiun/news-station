b19139d8778e12c77fdfd630ee36f4f19ed0eee4
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-04-08 20:53:00 +0800

    commit weather widget

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pick-your-games'),
+      dir: require('path').join(__dirname, './coverage/news-station'),
       subdir: '.',
       reporters: [
         { type: 'html' },
